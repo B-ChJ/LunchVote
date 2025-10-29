54e00ec84ad36ce5b3076c41ae9be1c9bb74daab
B-ChJ <bchaejoo@gmail.com>
2025-10-29 12:47:04 +0900

    Validation 검증 로직 추가

--- src/main/java/com/sparta/lunchvote/dto/CreateUserRequest.java
+++ src/main/java/com/sparta/lunchvote/dto/CreateUserRequest.java
@@ -1,10 +1,18 @@
 package com.sparta.lunchvote.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreateUserRequest {
+    @NotBlank(message = "이메일 입력은 필수입니다.")
+    @Email
     private String email;
+    @Size(min = 8, message = "비밀번호는 8자 이상 입력해야 합니다.")
+    @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
+    @NotBlank(message = "이름을 입력해 주세요.")
     private String name;
 }
